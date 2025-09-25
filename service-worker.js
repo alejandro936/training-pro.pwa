5f17e0c163d456255b3dce26311265ad8f813905
alejandro936 <alejandro@coach-alexlopez.com>
2025-09-25 12:57:09 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,3 @@
-self.addEventListener("install", (e)=>self.skipWaiting());
-self.addEventListener("activate", (e)=>self.clients.claim());
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+// Sin cachés manuales: siempre la última versión desde el servidor
