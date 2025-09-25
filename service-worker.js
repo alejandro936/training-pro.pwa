45cc656f6e79f608db57a68b6990776447a0e011
alejandro936 <alejandro@coach-alexlopez.com>
2025-09-25 13:34:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-// SW "reset": sin cachés, siempre última versión
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => self.clients.claim());
-self.addEventListener('fetch', () => {}); // no intercepta nada
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+// sin fetch handler → no cachea nada
